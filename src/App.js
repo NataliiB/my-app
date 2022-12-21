9e314de360ae379c2b6efb29c992f3c10d5395b1
natalyfriend <natalyfriend@ukr.net>
2022-12-21 14:57:55 +0200

    Resolving conflicts

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Nav from './components/Navbar/Nav';
-<<<<<<< HEAD
 import ProfileContainer from './components/Profile/ProfileContainer';
-=======
-import Profile from './components/Profile/Profile';
->>>>>>> f535ae7... Adding usersProfile page
 import { Route, Routes } from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import DialogesContainer from './components/Dialoges/DialogesContainer';
-<<<<<<< HEAD
-import UsersContainer from './components/UsersPage/UsersContainer';
-=======
 import UsersConnectApi from './components/UsersPage/UsersConnectApi';
 import UsersProfileContainer from './components/UsersProfilePage/UsersProfileContainer';
->>>>>>> f535ae7... Adding usersProfile page
+
 
 
 
@@ -33,12 +26,8 @@ const App = (props) => {
           <Route path='/news' element={<News/>}/>
           <Route path='/music' element={<Music/>}/>
           <Route path='/settings' element={<Settings/>}/>
-<<<<<<< HEAD
-          <Route path='/users' element={<UsersContainer/>}/>
-=======
           <Route path='/users' element={<UsersConnectApi/>}/>
           <Route path='/usersProfile' element={<UsersProfileContainer/>}/>
->>>>>>> f535ae7... Adding usersProfile page
         </Routes>
         </div>
         
