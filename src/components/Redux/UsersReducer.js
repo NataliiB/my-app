101aac1253cf8edcf4876fbfd33ecda337e9a5ae
natalyfriend <natalyfriend@ukr.net>
2022-12-21 15:08:59 +0200

    Continue resolving conflicts

--- src/components/Redux/UsersReducer.js
+++ src/components/Redux/UsersReducer.js
@@ -3,11 +3,7 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
 const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
-<<<<<<< HEAD
-const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
-=======
-const SET_USERS_PROFILE = 'SET_USERS_PROFILE';
->>>>>>> f535ae7... Adding usersProfile page
+
 
 let initialState = {
   usersData: [
@@ -26,13 +22,9 @@ let initialState = {
   ],
   pageSize : 2,
   totalUsersCount:3,
-<<<<<<< HEAD
-  currentPage : 1,
-  isFetching : false
-=======
   currentPage : 2,
   profile : []
->>>>>>> f535ae7... Adding usersProfile page
+
 
 };
 
@@ -61,25 +53,6 @@ export const usersReducer = (state = initialState, action) => {
     case SET_USERS:
       return {
         ...state,
-<<<<<<< HEAD
-        usersData: [action.users]
-      }
-    //   case SET_CURRENT_PAGE:
-    //     return {
-    //     ...state,
-    //     currentPage:action.currentPage
-    //     }
-    //     case SET_TOTAL_USERS_COUNT:
-    //       return {
-    //         ...state,
-    //         totalUsersCount : action.totalUsersCount
-    //       }
-    //       case TOGGLE_IS_FETCHING:
-    //         return {
-    //           ...state,
-    //           isFetching : action.isFetching
-    //         }
-=======
         usersData: [...state.usersData, ...action.usersData]
       }
       case SET_CURRENT_PAGE:
@@ -97,23 +70,14 @@ export const usersReducer = (state = initialState, action) => {
               ...state,
               profile : [...state.profile, action.profile]
             }
->>>>>>> f535ae7... Adding usersProfile page
     default:
       return state;
   }
 };
-<<<<<<< HEAD
-export const follow = (userId) => ({ type: FOLLOW, userId });
-export const unfollow = (userId) => ({ type: UNFOLLOW, userId });
-export const setUsers = (users) => ({ type: SET_USERS, users});
-export const setCurrentPage = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage});
-export const setTotalUsersCount = (totalUsersCount)=> ({type : SET_TOTAL_USERS_COUNT,totalUsersCount});
-export const toggleIsFetching = (isFetching)=> ({type: TOGGLE_IS_FETCHING,isFetching})
-=======
 export const followAC = (userId) => ({ type: FOLLOW, userId });
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId });
 export const setUsersAC = (users) => ({ type: SET_USERS, users});
 export const setCurrentPageAC = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage});
 export const setUsersProfileAC =(profile) => ({type: SET_USERS_PROFILE, profile})
->>>>>>> f535ae7... Adding usersProfile page
+
 export default usersReducer;
